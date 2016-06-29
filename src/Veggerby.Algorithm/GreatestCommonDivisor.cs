fb6c2507785dcf1a613afba2ee20b94354139eca
Jesper Veggerby <jesper@veggerby.com>
2016-06-29 15:36:07 +0200

    added euclid gcd algorithm

---
+++ src/Veggerby.Algorithm/GreatestCommonDivisor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Veggerby.Algorithm
+{
+    public class GreatestCommonDivisor
+    {
+        public static int Euclid(int a, int b)
+        {
+            while (b != 0)
+            {
+                var t = b;
+                b = a % b;
+                a = t;
+            }
+
+            return a;
+        }
+    }
+}
---
+++ test/Veggerby.Algorithm.Test/GreatestCommonDivisorTests.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Veggerby.Algorithm;
+using Xunit;
+
+namespace Veggerby.Algorithm.Test
+{
+    public class GreatestCommonDivisorTests
+    {
+        public class Euclid
+        {
+            [Theory]
+            [InlineData(9, 6, 3)]
+            [InlineData(71, 4, 1)]
+            [InlineData(1000, 2, 2)]
+            [InlineData(81, 36, 9)]
+            [InlineData(1071, 462, 21)]
+            public void Should_return_correct_gcd(int a, int b, int expected)
+            {
+                // arrange
+
+                // act
+                var actual = GreatestCommonDivisor.Euclid(a, b);
+
+                // assert
+                Assert.Equal(expected, actual);
+            }        
+        }
+    }
+}
