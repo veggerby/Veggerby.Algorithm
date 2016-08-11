ee201cc7e7791590e8433e6b81396e2eba3ecf08
Jesper Veggerby <jesper@veggerby.com>
2016-08-11 23:02:18 +0200

    added operator implementation for operands

--- src/Veggerby.Algorithm/Arithmetic/Operand.cs
+++ src/Veggerby.Algorithm/Arithmetic/Operand.cs
@@ -4,5 +4,70 @@ namespace Veggerby.Algorithm.Arithmetic
     {
         public abstract double Evaluate(OperationContext context);
         public abstract void Accept(IOperandVisitor visitor);
+
+        public static Operand operator +(Operand left, Operand right)
+        {
+            if (left.Equals(right))
+            {
+                return 2 * left;
+            }
+
+            return new Addition(left, right);
+        }
+
+        public static Operand operator -(Operand left, Operand right)
+        {
+            if (left.Equals(right))
+            {
+                return 0;
+            }
+
+            return new Subtraction(left, right);
+        }
+
+        public static Operand operator *(Operand left, Operand right)
+        {
+            if (left.Equals(right))
+            {
+                return left ^ 2;
+            }
+
+            return new Multiplication(left, right);
+        }
+
+        public static Operand operator /(Operand left, Operand right)
+        {
+            if (left.Equals(right))
+            {
+                return 1;
+            }
+
+            return new Division(left, right);
+        }
+
+        public static Operand operator ^(Operand left, Operand right)
+        {
+            if (left.Equals(new Constant(1)) || right.Equals(new Constant(0)))
+            {
+                return 1;
+            }
+
+            if (right.Equals(new Constant(1)))
+            {
+                return left;
+            }
+
+            return new Power(left, right);
+        }
+
+        public static implicit operator Operand(int value)
+        {
+            return new Constant(value);
+        }
+
+        public static implicit operator Operand(double value)
+        {
+            return new Constant(value);
+        }
     }
 }
---
+++ test/Veggerby.Algorithm.Tests/Arithmetic/OperandTests.cs
@@ -0,0 +1,234 @@
+using Shouldly;
+using Veggerby.Algorithm.Arithmetic;
+using Xunit;
+
+namespace Veggerby.Algorithm.Tests.Arithmetic
+{
+    public class OperandTests
+    {
+        public class OperatorAdd
+        {
+            [Fact]
+            public void Should_return_addition()
+            {
+                // arrange
+                var left = new Constant(3);
+                var right = new Variable("x");
+
+                // act
+                var actual = left + right;
+                
+                // assert
+                actual.ShouldBeOfType<Addition>();
+                ((BinaryOperation)actual).Left.ShouldBe(left);
+                ((BinaryOperation)actual).Right.ShouldBe(right);
+            }
+
+            [Fact]
+            public void Should_return_self_times_2()
+            {
+                // arrange
+                var left = new Variable("x");
+                var right = new Variable("x");
+
+                // act
+                var actual = left + right;
+                
+                // assert
+                actual.ShouldBeOfType<Multiplication>();
+                ((BinaryOperation)actual).Left.ShouldBe(new Constant(2));
+                ((BinaryOperation)actual).Right.ShouldBe(right);
+            }
+        }
+
+        public class OperatorSubtract
+        {
+            [Fact]
+            public void Should_return_subtraction()
+            {
+                // arrange
+                var left = new Constant(3);
+                var right = new Variable("x");
+
+                // act
+                var actual = left - right;
+                
+                // assert
+                actual.ShouldBeOfType<Subtraction>();
+                ((BinaryOperation)actual).Left.ShouldBe(left);
+                ((BinaryOperation)actual).Right.ShouldBe(right);
+            }
+
+            [Fact]
+            public void Should_return_zero()
+            {
+                // arrange
+                var left = new Variable("x");
+                var right = new Variable("x");
+
+                // act
+                var actual = left - right;
+                
+                // assert
+                actual.ShouldBe(new Constant(0));
+            }
+        }
+
+        public class OperatorMultiplication
+        {
+            [Fact]
+            public void Should_return_multiplication()
+            {
+                // arrange
+                var left = new Constant(3);
+                var right = new Variable("x");
+
+                // act
+                var actual = left * right;
+                
+                // assert
+                actual.ShouldBeOfType<Multiplication>();
+                ((BinaryOperation)actual).Left.ShouldBe(left);
+                ((BinaryOperation)actual).Right.ShouldBe(right);
+            }
+
+            [Fact]
+            public void Should_return_self_squared()
+            {
+                // arrange
+                var left = new Variable("x");
+                var right = new Variable("x");
+
+                // act
+                var actual = left * right;
+                
+                // assert
+                actual.ShouldBeOfType<Power>();
+                ((BinaryOperation)actual).Left.ShouldBe(right);
+                ((BinaryOperation)actual).Right.ShouldBe(new Constant(2));
+            }
+        }
+
+        public class OperatorDivision
+        {
+            [Fact]
+            public void Should_return_division()
+            {
+                // arrange
+                var left = new Constant(3);
+                var right = new Variable("x");
+
+                // act
+                var actual = left / right;
+                
+                // assert
+                actual.ShouldBeOfType<Division>();
+                ((BinaryOperation)actual).Left.ShouldBe(left);
+                ((BinaryOperation)actual).Right.ShouldBe(right);
+            }
+
+            [Fact]
+            public void Should_return_zero()
+            {
+                // arrange
+                var left = new Variable("x");
+                var right = new Variable("x");
+
+                // act
+                var actual = left / right;
+                
+                // assert
+                actual.ShouldBe(new Constant(1));
+            }
+        }
+
+        public class OperatorPower
+        {
+            [Fact]
+            public void Should_return_power()
+            {
+                // arrange
+                var left = new Constant(3);
+                var right = new Variable("x");
+
+                // act
+                var actual = left ^ right;
+                
+                // assert
+                actual.ShouldBeOfType<Power>();
+                ((BinaryOperation)actual).Left.ShouldBe(left);
+                ((BinaryOperation)actual).Right.ShouldBe(right);
+            }
+
+            [Fact]
+            public void Should_return_one_for_one_as_base()
+            {
+                // arrange
+                var left = new Constant(1);
+                var right = new Variable("x");
+
+                // act
+                var actual = left ^ right;
+                
+                // assert
+                actual.ShouldBe(new Constant(1));
+            }
+
+            [Fact]
+            public void Should_return_one_for_zero_as_exponent()
+            {
+                // arrange
+                var left = new Variable("x");
+                var right = new Constant(0);
+
+                // act
+                var actual = left ^ right;
+                
+                // assert
+                actual.ShouldBe(new Constant(1));
+            }
+
+
+            [Fact]
+            public void Should_return_baze_for_one_as_exponent()
+            {
+                // arrange
+                var left = new Variable("x");
+                var right = new Constant(1);
+
+                // act
+                var actual = left ^ right;
+                
+                // assert
+                actual.ShouldBe(left);
+            }
+        }
+
+        public class Implicit
+        {
+            [Fact]
+            public void Should_create_constant_from_int()
+            {
+                // arrange
+                
+                // act
+                var actual = (Constant)3;
+                
+                // assert
+                actual.Value.ShouldBe(3);
+            }
+
+            [Fact]
+            public void Should_create_constant_from_double()
+            {
+                // arrange
+                
+                // act
+                var actual = (Constant)3.4;
+                
+                // assert
+                actual.Value.ShouldBe(3.4);
+            }
+        }
+    }
+}
