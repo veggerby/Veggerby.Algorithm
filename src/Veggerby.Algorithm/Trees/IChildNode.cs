e1eef4b3082c538c8c149a5ade98d56612beae57
Jesper Veggerby <jesper@veggerby.com>
2017-06-21 10:44:42 +0200

    moved isleaf to childnode

--- src/Veggerby.Algorithm/Trees/IChildNode.cs
+++ src/Veggerby.Algorithm/Trees/IChildNode.cs
@@ -6,7 +6,7 @@ namespace Veggerby.Algorithm.Trees
     public interface IChildNode<TParent> where TParent : IParentNode
     {
         TParent Parent { get; }
-
         void SetParent(IParentNode parent);
+        bool IsLeaf { get; }
     }
 }
--- src/Veggerby.Algorithm/Trees/INode.cs
+++ src/Veggerby.Algorithm/Trees/INode.cs
@@ -6,6 +6,5 @@ namespace Veggerby.Algorithm.Trees
     public interface INode<T>
     {
         T Payload { get; set; }
-        bool IsLeaf { get; }
     }
 }
